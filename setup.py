7148a113bbc938ef5902fa5474788e58f1546286
Ryan Miller <ryan@devopsmachine.com>
2018-07-24 13:29:27 -0400

    New Release - 1.1.0 (#3)
    * Updated version number
    
    * Added debugging keyword
    
    * Updated to version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='conlog',
     packages=['conlog'],
-    version='0.3',
+    version='1.1.0',
     install_requires=['PyYAML>=3.12', 'bitmath>=1.3.1.2', 'six>=1.10.0'],
     description='Anti-Boilerplate Console Logging Module for Python',
     long_description=open('README.rst').read(),
@@ -10,10 +10,9 @@ setup(
     author_email='ryan@devopsmachine.com',
     license='MIT',
     url='https://github.com/RyanMillerC/conlog',
-    download_url='https://github.com/RyanMillerC/conlog/archive/0.3.tar.gz',
-    keywords=['logging', 'yaml'],
+    download_url='https://github.com/RyanMillerC/conlog/archive/1.1.0.tar.gz',
+    keywords=['logging', 'debugging'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
